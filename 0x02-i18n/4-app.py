467b0269184cdda3eb7678aea4862758d643b2c8
Youssef Rhaimouni <youssefgf123@gmail.com>
2024-07-30 12:26:18 -0700

    Task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" 4. Force locale with URL parameter """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Config class to define """
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Get locale """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """ A default way """
+    return render_template("4-index.html")
+
+if __name__ == "__main__":
+    app.run()
